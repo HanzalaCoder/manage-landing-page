30db812eea4564de3138169f2ec0263eb2d2bc46
Hanzala Khan <hanzalacoder061@gmail.com>
2023-08-13 20:19:00 +0500

    add css test

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Section2 from './components/Section2'
 function App() {
 
   return (
-    <div>
+    <div className="font-pro box-border">
       <header>
           <Header />
       </header>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,8 +2,9 @@
 
 export default function Button() {
     return (
-        <button>
-            Get Startrd
+        <button className="bg-BrightRed  text-VaryLightGray 
+        py-4 px-12 rounded-full shadow hover:bg-BrightRed/50">
+            Get Started
         </button>
 
 
